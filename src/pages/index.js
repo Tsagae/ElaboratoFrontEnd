06b9f87371a31be3d308f0c6a730a005e012cde2
Tsagae <matteo2309@hotmail.com>
2021-05-11 14:41:23 +0200

    created new index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import * as React from "react";
 import "../globalStyle/globalStyle.css";
-import Layout from "../components/layout";
-
+import Layout from "../components/Layout";
+import Searchbar from "../components/Searchbar";
+import Graph from "../components/Graph";
 const IndexPage = () => {
   return (
     <div>
       <Layout>
         <div>
           <h1>Content</h1>
+          <Searchbar />
+          <Graph source="blabla">OOOOO</Graph>
         </div>
       </Layout>
     </div>
