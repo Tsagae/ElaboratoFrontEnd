cb2401632fc4ab3d1501c8b6af03d9c07c86bd0a
Tsagae <matteo2309@hotmail.com>
2021-05-11 18:31:58 +0200

    updated search bar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,13 +2,23 @@ import { Link } from "gatsby";
 import * as React from "react";
 import * as styles from "./Searchbar.module.css";
 import "../globalStyle/globalStyle.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Searchbar({ children }) {
   return (
     <div className={styles.container}>
       <form>
-        <input type="search" className={styles.bar}></input>
+        <input type="search" className={styles.bar} placeholder="Scrivi qua" />
       </form>
+      <button>
+        <StaticImage
+          src="../../public/images/searchIcon.png"
+          placeholder="blurred"
+          layout="fixed"
+          width={50}
+          height={50}
+        />
+      </button>
     </div>
   );
 }
