801e93419595f6e510290c1b4b33fbec3743b32d
Tsagae <matteo2309@hotmail.com>
2021-05-12 20:42:14 +0200

    updated charts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import * as React from "react";
 import "../globalStyle/globalStyle.css";
 import Layout from "../components/Layout";
 import Searchbar from "../components/Searchbar";
-import Graph from "../components/Graph";
+import Chart from "../components/Chart";
+import HistoricalChart from "../components/HistoricalChart";
 import * as styles from "./index.module.css";
 
 const IndexPage = () => {
@@ -12,12 +13,12 @@ const IndexPage = () => {
         <div>
           <h1>Content</h1>
           <Searchbar />
-          <div className={styles.mainGraphs}>
-            <div className={styles.graphWrapper}>
-              <Graph source="" />
+          <div className={styles.mainCharts}>
+            <div className={styles.chartWrapper}>
+              <Chart source="" />
             </div>
-            <div className={styles.graphWrapper}>
-              <Graph source="" />
+            <div className={styles.chartWrapper}>
+              <HistoricalChart source="" />
             </div>
           </div>
         </div>
