6e9a486ad867ca1a22934ccda1b060d1dc1d7b6f
Tsagae <matteo2309@hotmail.com>
2021-05-19 18:54:40 +0200

    updated game page

--- src/pages/game.js
+++ src/pages/game.js
@@ -4,11 +4,13 @@ import Layout from "../components/Layout";
 import Searchbar from "../components/Searchbar";
 import TwitchThumbnail from "../components/TwitchThumbnail";
 import GamesIndex from "../components/GamesIndex";
+import GameInfoChart from "../components/GameInfoChart";
 
 export default class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            gameID: 0,
             loaded: false,
             EsportGames_Data: [],
             HistoricalEsportData_Data: [],
@@ -20,17 +22,18 @@ export default class Game extends React.Component {
         let search = window.location.search;
         let params = new URLSearchParams(search);
         let gameID = params.get('gameID');
-        //console.log(gameID);
-        fetch("https://mzaghenoapi.sytes.net/queryDB/getGameInfo?GameID=" + gameID)
+        console.log(gameID);
+        fetch("https://mzaghenoapi.sytes.net/queryDB/getGameInfo?gameID=" + gameID)
             .then((res) => res.text())
             .then((data) => {
                 this.setState({
+                    gameID: gameID,
                     loaded: true,
                     EsportGames_Data: JSON.parse(data)[0][0],
-                    HistoricalEsportData_Data: JSON.parse(data)[0][1],
-                    TwitchEsportGames_Data: JSON.parse(data)[0][2],
+                    HistoricalEsportData_Data: JSON.parse(data)[1],
+                    TwitchEsportGames_Data: JSON.parse(data)[2],
                 });
-                //console.log(JSON.parse(data));
+                console.log(this.state);
             });
     }
 
@@ -46,15 +49,21 @@ export default class Game extends React.Component {
                 </div>
             );
         } else {
+            console.log(this.state.TwitchEsportGames_Data);
             return (
                 <div>
                     <Layout>
-                        <div className={styles.mainContent}>
-                            <div className={styles.generalGameInfo}>
+                        <div className={styles.topInfoCard}>
+                            <div className={styles.infoBox}>
                                 <TwitchThumbnail game={this.state.EsportGames_Data.GameName} width={198} height={264} />
+                                <div className={styles.generalGameInfo}>
+                                    <p>{this.state.EsportGames_Data.GameName}</p>
+                                    <p>Release date: {this.state.EsportGames_Data.ReleaseDate}</p>
+                                    <p>Genre: {this.state.EsportGames_Data.Genre}</p>
+                                </div>
                             </div>
-                            <div>
-
+                            <div className={styles.gameInfoChartContainer}>
+                                <GameInfoChart className={styles.gameInfoChart} twitchData={this.state.TwitchEsportGames_Data} historicalEsportData={this.state.HistoricalEsportData_Data} />
                             </div>
                         </div>
                     </Layout>
