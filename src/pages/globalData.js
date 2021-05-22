c6b0ae089d1592c69faf61a03f8bb0c39238afda
Tsagae <matteo2309@hotmail.com>
2021-05-23 01:44:38 +0200

    added map lazy load

--- src/pages/globalData.js
+++ src/pages/globalData.js
@@ -1,13 +1,15 @@
 import * as React from "react";
+import { Suspense } from "react";
 import "../globalStyle/globalStyle.css";
 import Layout from "../components/Layout";
+import Loader from "../components/Loader";
 import Searchbar from "../components/Searchbar";
 import Chart from "../components/Chart";
 import HistoricalChart from "../components/HistoricalChart";
 import MostViewed12Months from "../components/MostViewed12MonthsGraph";
 import MostPlayed12Months from "../components/MostPlayed12MonthsGraph";
 import HighestEarningPlayers from "../components/HighestEarningPlayersChart";
-import WsdmHighestEarningCountriesMap from "../components/WsdmHighestEarningCountriesMap";
+//import WsdmHighestEarningCountriesMap from "../components/WsdmHighestEarningCountriesMap";
 import * as styles from "./globalData.module.css";
 
 //could be remade by movinig the api request here by making GlobalData a class and then passing data as props to the charts
@@ -20,6 +22,9 @@ export default class GlobalData extends React.Component {
 
 
   render() {
+
+    const WsdmHighestEarningCountriesMap = React.lazy(() => import("../components/WsdmHighestEarningCountriesMap"));
+
     return (
       <div>
         <Layout>
@@ -50,12 +55,14 @@ export default class GlobalData extends React.Component {
                   <HighestEarningPlayers className={styles.chart2} />
                 </div>
               </div>
-              <div className={styles.mapContainer}>
-                <h4>Highest Earning Countries</h4>
-                <div className={styles.mapWrapper}>
-                  <WsdmHighestEarningCountriesMap width={"100%"} heigth={"auto"} />
+              <Suspense fallback={<Loader/>}>
+                <div className={styles.mapContainer}>
+                  <h4>Highest Earning Countries</h4>
+                  <div className={styles.mapWrapper}>
+                    <WsdmHighestEarningCountriesMap width={"100%"} heigth={"auto"} />
+                  </div>
                 </div>
-              </div>
+              </Suspense>
             </div>
           </div>
         </Layout>
