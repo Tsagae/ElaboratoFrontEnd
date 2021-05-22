28e234e21bc087cd424e0b60341312ab3eb32d1b
Tsagae <matteo2309@hotmail.com>
2021-05-23 01:44:23 +0200

    created simple loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "../globalStyle/globalStyle.css";
+
+export default function Loader({ children }) {
+  return (
+    <div>
+        <h4>Loading...</h4>
+    </div>
+  );
+}
