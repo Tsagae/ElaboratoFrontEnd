45a4fb00f6a1bcf2cfe8f8151c53f99dbc594e7e
Tsagae <matteo2309@hotmail.com>
2021-05-22 19:56:31 +0200

    now wrapped in React Fragment

--- src/components/MapWithTooltip.js
+++ src/components/MapWithTooltip.js
@@ -1,17 +1,8 @@
 import * as React from "react";
 import "../globalStyle/globalStyle.css";
-import Layout from "../components/Layout";
-import Searchbar from "../components/Searchbar";
-import Chart from "../components/Chart";
-import HistoricalChart from "../components/HistoricalChart";
-import MostViewed12Months from "../components/MostViewed12MonthsGraph";
-import MostPlayed12Months from "../components/MostPlayed12MonthsGraph";
-import HighestEarningPlayers from "../components/HighestEarningPlayersChart";
-import * as styles from "./globalData.module.css";
 import HighestEarningPlayersMap from "../components/HighestEarningPlayersMap";
-import ReactDOM from "react-dom";
 import ReactTooltip from "react-tooltip";
-
+import WsdmHighestEarningPlayersMap from "../components/WsdmHighestEarningPlayersMap";
 
 //could be remade by movinig the api request here by making GlobalData a class and then passing data as props to the charts
 export default class MapWithTooltip extends React.Component {
@@ -30,10 +21,10 @@ export default class MapWithTooltip extends React.Component {
 
     render() {
         return (
-            <div>
-                <HighestEarningPlayersMap setTooltipContent={this.setTooltipContent.bind(this)} />
-                <ReactTooltip>{this.state.tooltipContent}</ReactTooltip>
-            </div>
+            <React.Fragment>
+                <ReactTooltip>"Ciao"</ReactTooltip>
+                <WsdmHighestEarningPlayersMap setTooltipContent={this.setTooltipContent.bind(this)} />
+            </React.Fragment>
         );
     }
 }
