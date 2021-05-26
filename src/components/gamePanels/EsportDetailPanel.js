256b2dc33692d0f82c1f60e07333dd47df5cee29
Tsagae <matteo2309@hotmail.com>
2021-05-27 01:16:44 +0200

    added rounding

--- src/components/gamePanels/EsportDetailPanel.js
+++ src/components/gamePanels/EsportDetailPanel.js
@@ -2,20 +2,35 @@ import * as React from "react";
 import * as styles from "./EsportDetailPanel.module.css";
 
 export default function EsportDetailPanel(props) {
+    
+    function rounded(num) {
+        if (typeof num == 'undefined') {
+            return 0;
+        } else if (num > 1000000000) {
+            return Math.round(num / 100000000) / 10 + "Bn";
+        } else if (num > 1000000) {
+            return Math.round(num / 100000) / 10 + "M";
+        } else if (num > 1000) {
+            return Math.round(num / 100) / 10 + "K";
+        } else {
+            return num;
+        }
+    };
+   
     return (
         <div className={styles.container}>
             <div>
-                <p>{props.EsportGames_Data.TotalEarnings}</p>
+                <p>{rounded(props.EsportGames_Data.TotalEarnings)}</p>
                 <hr />
                 <h5>Total Earnings</h5>
             </div>
             <div >
-                <p>{props.EsportGames_Data.OfflineEarnings}</p>
+                <p>{rounded(props.EsportGames_Data.OfflineEarnings)}</p>
                 <hr />
                 <h5>Offline Earnings</h5>
             </div>
             <div >
-                <p>{props.EsportGames_Data.TotalPlayers}</p>
+                <p>{rounded(props.EsportGames_Data.TotalPlayers)}</p>
                 <hr />
                 <h5>Total Players</h5>
             </div>
--- src/components/gamePanels/TwitchDetailPanel.js
+++ src/components/gamePanels/TwitchDetailPanel.js
@@ -2,20 +2,35 @@ import * as React from "react";
 import * as styles from "./TwitchDetailPanel.module.css";
 
 export default function TwitchDetailPanel(props) {
+    
+    function rounded(num) {
+        if (typeof num == 'undefined') {
+            return 0;
+        } else if (num > 1000000000) {
+            return Math.round(num / 100000000) / 10 + "Bn";
+        } else if (num > 1000000) {
+            return Math.round(num / 100000) / 10 + "M";
+        } else if (num > 1000) {
+            return Math.round(num / 100) / 10 + "K";
+        } else {
+            return num;
+        }
+    };
+    
     return (
         <div className={styles.container}>
             <div>
-                <p>{props.TwitchDataSummary.Peak_Viewers}</p>
+                <p>{rounded(props.TwitchDataSummary.Peak_Viewers)}</p>
                 <hr />
                 <h5>Peak Viewers</h5>
             </div>
             <div >
-                <p>{props.TwitchDataSummary.Avg_Viewers}</p>
+                <p>{rounded(props.TwitchDataSummary.Avg_Viewers)}</p>
                 <hr />
                 <h5>Average Viewers</h5>
             </div>
             <div >
-                <p>{props.TwitchDataSummary.Peak_Channels}</p>
+                <p>{rounded(props.TwitchDataSummary.Peak_Channels)}</p>
                 <hr />
                 <h5>Peak Channels</h5>
             </div>
